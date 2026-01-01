164fb97acf65c2fcf17120990e1fb8b38e65acdf
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2026-01-02 02:11:30 +0600

    usuals added  to checkout

--- Final/Project/checkout.php
+++ Final/Project/checkout.php
@@ -9,6 +9,12 @@
 
 <center><h1>Checkout</h1></center>
 
+
+<a href="dashboard.php">
+<img src="images/logo.png" id="logo">
+</a>
+
+
 <?php
 $address = "";
 $phonenumber = "";
@@ -64,6 +70,28 @@ if ($error != "") {
     echo "<p style='color:red; text-align:center;'>$error</p>";
 }
 ?>
+
+
+
+<a href="user1.html">
+    <img src="images/user1.png" id="usericon"
+         style="width:60px; height:60px; position:absolute; top:10px; right:70px; z-index:1000; border:3px solid red;">
+</a>
+
+
+<div id="footer" style="
+       text-align: center;
+    color: white;
+    margin-top: 100px;
+    padding: 20px;
+    font-size: 14px;
+    opacity: 0.7;
+">
+    <p>© 2025 NG Auto. All rights reserved.</p>
+    <p>Contact: support@ngauto.com | +880-111-222-333</p>
+</div>
+
+
 
 </body>
 </html>
