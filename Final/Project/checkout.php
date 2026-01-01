de1f4e6be07eaa10f85ff39efe7c16aa6902b6a9
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2026-01-02 01:59:54 +0600

    checkout page started. added form

--- Final/Project/checkout.php
+++ Final/Project/checkout.php
@@ -7,6 +7,21 @@
 </head>
 <body>
     
+    <h2>Checkout</h2>
+
+<form method="post" action="">
+
+    <label>Address:</label><br>
+    <textarea name="address" rows="4" cols="40"></textarea>
+    <br><br>
+
+    <label>Phone Number:</label><br>
+    <input type="text" name="phonenumber">
+    <br><br>
+
+    <input type="submit" name="submit" value="Place Order">
+
+</form>
 
 
 
