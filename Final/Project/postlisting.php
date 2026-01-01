a6337ae8635f329fa52850a1f3ffaf543064ebd4
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2026-01-01 14:16:12 +0600

    added common elements like footer logo user

--- Final/Project/postlisting.php
+++ Final/Project/postlisting.php
@@ -10,6 +10,11 @@
 <h1>Sell Your Car</h1>
 
 
+<a href="dashboard.php">
+    <img src="images/logo.png" id="logo">
+</a>
+
+
 
 
 
@@ -211,6 +216,28 @@ if ($error != "") {
     echo "<p style='color:red;'>$error</p>";
 }
 ?>
+
+
+
+<a href="user1.html">
+    <img src="images/user1.png" id="usericon"
+         style="width:60px; height:60px; position:absolute; top:10px; right:70px; z-index:1000; border:3px solid red;">
+</a>
+
+
+<div id="footer" style="
+       text-align: center;
+    color: white;
+    margin-top: 100px;
+    padding: 20px;
+    font-size: 14px;
+    opacity: 0.7;
+">
+    <p>© 2025 NG Auto. All rights reserved.</p>
+    <p>Contact: support@ngauto.com | +880-111-222-333</p>
+</div>
+
+
 
 </body>
 </html>
