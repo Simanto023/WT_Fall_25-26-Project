e6d8b73992c27680f6c6988779cb304b942862f1
Saidur Rahman <23-51985-2@student.aiub.edu>
2026-01-02 22:10:39 +0600

    Add category logic added in add_category.php using proper database connection

--- Project/PHP/add_category.php
+++ Project/PHP/add_category.php
@@ -1,1 +1,23 @@
-//empty
+<?php
+include __DIR__ . "/../DB/db.php";
+//logic to redirect to manage_cars if link opened directly
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header("Location: ../manage_category.php");
+    exit;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = $_POST["category_name"];
+
+    if (empty($name)) {
+        header("Location: ../manage_category.php?error=1");
+        exit;
+    }
+
+    $sql = "INSERT INTO categories (name) VALUES ('$name')";
+    $conn->query($sql);
+
+    header("Location: ../manage_category.php");
+    exit;
+}
+?>
