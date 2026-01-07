898c38570f0e16f1b13298d49600e3f4300223fd
Saidur Rahman <23-51985-2@student.aiub.edu>
2026-01-08 00:17:07 +0600

    Forgot to add remove cookie in logout

--- Project/PHP/logout.php
+++ Project/PHP/logout.php
@@ -5,5 +5,9 @@ session_unset();
 
 session_destroy();
 
+if (isset($_COOKIE['remember_user'])) {
+    setcookie("remember_user", "", time() - 3600, "/");
+}
+
 header("Location: ../login.php");
 exit;
