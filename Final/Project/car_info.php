c6094cd200a2060645d87bd2f79348496cb4f971
FairoozAlamMahi <23-51878-2@student.aiub.edu>
2026-01-09 11:01:40 +0600

    made the car info page. only the html and css.. will work on the 2 buttons later..

---
+++ Final/Project/car_info.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Car Info</title>
+    <link rel="stylesheet" href="CSS/car_info.css">
+</head>
+<body>
+
+<a href="dashboard.php">
+    <img src="images/logo.png" id="logo">
+</a>
+
+<a href="user1.html">
+    <img src="images/user1.png" id="usericon"
+         style="width:60px; height:60px; position:absolute; top:10px; right:70px;">
+</a>
+
+<h1>Car Details</h1>
+
+<div id="car_container">
+
+    <div id="car_image_box">
+        <img src="images/cars/sample.jpg" id="car_image">
+    </div>
+
+    <div id="car_info_box">
+        <h2>Car Name</h2>
+
+        <p><b>Brand:</b> Brand Name</p>
+        <p><b>Model:</b> Model Name</p>
+        <p><b>Color:</b> Color</p>
+        <p><b>Price:</b> Price</p>
+        <p><b>Horsepower:</b> HP</p>
+        <p><b>Engine Capacity:</b> Engine</p>
+        <p><b>Transmission:</b> Transmission</p>
+        <p><b>Category:</b> Category</p>
+
+        <br>
+
+        <button id="buy_btn">Buy Now</button>
+        <button id="compare_btn">Compare</button>
+    </div>
+
+</div>
+
+<div id="footer">
+    <p>© 2025 NG Auto. All rights reserved.</p>
+    <p>Contact: support@ngauto.com | +880-111-222-333</p>
+</div>
+
+</body>
+</html>
